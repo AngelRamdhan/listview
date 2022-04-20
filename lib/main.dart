87edd4629a6521f410eab7a093ddb378701504da
AngelRamdhan <103801832+AngelRamdhan@users.noreply.github.com>
2022-04-20 21:28:41 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,161 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    title: "My Apps",
-    home: new Myapp(),
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 
-class Myapp extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: new Text("My Apps"),
-      ),
-      body: new Container(
-        child: ListView(
-          children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  Container(
+      backgroundColor: Colors.purple.shade300,
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 15.0, left: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+                Container(
                     width: 100.0,
-                    height: 100.0,
-                    color: Colors.blue,
-                    child: new Icon(
-                      Icons.home,
-                      color: Colors.white,
-                      size: 50.0,
-                    ),
-                  ),
-                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.filter_list),
+                          color: Colors.white,
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.menu),
+                          color: Colors.white,
+                          onPressed: () {},
+                        )
+                      ],
+                    ))
+              ],
+            ),
+          ),
+          SizedBox(height: 25.0),
+          Padding(
+            padding: EdgeInsets.only(left: 40.0),
+            child: Row(
+              children: <Widget>[
+                Text('Angel', style: TextStyle(fontFamily: 'Poppins', color: Colors.black, fontWeight: FontWeight.bold, fontSize: 25.0)),
+                SizedBox(width: 10.0),
+                Text('Food & Beverage', style: TextStyle(fontFamily: 'Poppins', color: Colors.white, fontSize: 25.0))
+              ],
+            ),
+          ),
+          SizedBox(height: 40.0),
+          Container(
+            height: MediaQuery.of(context).size.height - 185.0,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(0.0)),
+            ),
+            child: ListView(
+              primary: false,
+              padding: EdgeInsets.only(left: 25.0, right: 20.0),
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.only(top: 45.0),
                     child: Container(
-                      padding: EdgeInsets.all(15.0),
-                      height: 100.0,
-                      color: Colors.blue[200],
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Artikel 1", style: TextStyle(color: Colors.white, fontSize: 15.0)),
-                          Text(
-                            "Ini adalah contoh artikel pada listview dengan versi custom",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ],
+                        height: MediaQuery.of(context).size.height - 300.0,
+                        child: ListView(children: [
+                          _buildFoodItem('assets/martabakmanis1.jpg', 'Martabak Manis', '\Rp.20000'),
+                          _buildFoodItem('assets/martabak.jpg', 'Martabak Telor', '\Rp.10000'),
+                        ]))),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      height: 65.0,
+                      width: 60.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, style: BorderStyle.solid, width: 1.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Center(
+                        child: Icon(Icons.search, color: Colors.black),
                       ),
                     ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.blue,
-                    child: new Icon(
-                      Icons.home,
-                      color: Colors.white,
-                      size: 50.0,
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(15.0),
-                      height: 100.0,
-                      color: Colors.blue[200],
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Artikel 2", style: TextStyle(color: Colors.white, fontSize: 15.0)),
-                          Text(
-                            "Ini adalah contoh artikel pada listview dengan versi custom",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ],
+                    Container(
+                      height: 65.0,
+                      width: 60.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, style: BorderStyle.solid, width: 1.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Center(
+                        child: Icon(Icons.shopping_basket, color: Colors.black),
                       ),
                     ),
-                  )
-                ],
-              ),
+                    Container(
+                      height: 65.0,
+                      width: 120.0,
+                      decoration: BoxDecoration(border: Border.all(color: Colors.grey, style: BorderStyle.solid, width: 1.0), borderRadius: BorderRadius.circular(10.0), color: Color(0xFF1C1428)),
+                      child: Center(child: Text('Checkout', style: TextStyle(fontFamily: 'Poppins', color: Colors.white, fontSize: 15.0))),
+                    )
+                  ],
+                )
+              ],
             ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.blue,
-                    child: new Icon(
-                      Icons.home,
-                      color: Colors.white,
-                      size: 50.0,
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(15.0),
-                      height: 100.0,
-                      color: Colors.blue[200],
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Artikel 3", style: TextStyle(color: Colors.white, fontSize: 15.0)),
-                          Text(
-                            "Ini adalah contoh artikel pada listview dengan versi custom",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.blue,
-                    child: new Icon(
-                      Icons.home,
-                      color: Colors.white,
-                      size: 50.0,
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(15.0),
-                      height: 100.0,
-                      color: Colors.blue[200],
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Artikel 4", style: TextStyle(color: Colors.white, fontSize: 15.0)),
-                          Text(
-                            "Ini adalah contoh artikel pada listview dengan versi custom",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
+  }
+
+  Widget _buildFoodItem(String imgPath, String foodName, String price) {
+    return Padding(
+        padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+        child: InkWell(
+            onTap: () {},
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                    child: Row(children: [
+                  Hero(tag: imgPath, child: Image(image: AssetImage(imgPath), fit: BoxFit.cover, height: 75.0, width: 75.0)),
+                  SizedBox(width: 10.0),
+                  Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                    Text(foodName, style: TextStyle(fontFamily: 'Poppins', fontSize: 17.0, fontWeight: FontWeight.bold)),
+                    Text(price, style: TextStyle(fontFamily: 'Poppins', fontSize: 15.0, color: Colors.grey))
+                  ])
+                ])),
+                IconButton(icon: Icon(Icons.add), color: Colors.black, onPressed: () {})
+              ],
+            )));
   }
 }
